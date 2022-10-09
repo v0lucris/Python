7f7f2305aaa88a1708b8be2af398b0204ea7b16d
Tati <tanushka-p.94@mail.ru>
2022-10-09 19:58:28 +0300

    Lesson 1 task1 save 2

--- Lesson1Task1.py
+++ Lesson1Task1.py
@@ -4,6 +4,11 @@
 week = 'Понедельний вторник среда четверг пятница суббота воскресенье'
 weekstr = week.split()
 index = int(input("Write count: "))
-while index < (len(weekstr) + 2):
-    print(weekstr[index - 1])
-    break
+#while index < (len(weekstr) + 2):
+    #print(weekstr[index - 1])
+    #break
+
+if  weekstr[index - 1] == 'суббота' or weekstr[index - 1] == 'воскресенье':
+    print('Yes')
+else:
+    print('No')
